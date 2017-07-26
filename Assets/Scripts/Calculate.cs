66ec54509b73977398a8f883a2032b60733ac7a3
jmblixt3 <jmblixt3@gmail.com>
2017-07-26 17:20:36 -0500

    init
    weeee

---
+++ Assets/Scripts/Calculate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculate : MonoBehaviour {
+
+    
+    public Dropdown Gender_input;
+    public InputField input_1;
+    public InputField input_2;
+    public InputField input_3;
+    public Text CalculationOuput;
+
+    private static double val1;//Label
+    private static double val2;//Label
+    private static double val3;//Label
+    private static int valgen;
+    public void OnCalculateButtonClick()
+    {
+        ValueParse();
+
+        if(Gender())
+        {
+            switch(AgeGroup()){
+                case 1:
+                    CalculationOuput.text = "U18Male";
+                    break;
+                default:
+                 break;
+            }
+        }
+        else
+        {
+            CalculationOuput.text = "Girl";
+        }
+    }
+    private static Boolean Gender()
+    {
+        Boolean gender;//true male; false female
+        if(valgen == 0)
+        {
+            gender = true;
+            Debug.Log("Male");
+        }
+        else
+        {
+            gender = false;
+            Debug.Log("Female");
+        }
+        return gender;
+    }
+    private static int AgeGroup()
+    {
+        int ageGroup;
+        if(val1 < 18)
+        {
+            ageGroup = 1;
+        }else if(val1>=18 && val1 < 30)
+        {
+            ageGroup = 2;
+        }
+        else
+        {
+            ageGroup = 0;
+        }
+
+
+        return ageGroup;
+    }
+    private static Double Weight()
+    {
+        double weight;
+        weight = val2;
+
+
+        return weight;
+    }
+    private static Double Height()
+    {
+        double height;
+        height = val2;
+
+
+        return height;
+    }
+    private void ValueParse()
+    {
+        valgen = Gender_input.value;
+        try
+        {
+            val1 = double.Parse(input_1.text);
+            val2 = double.Parse(input_2.text);
+            val3 = double.Parse(input_3.text);
+
+        }
+        catch
+        {
+            CalculationOuput.text = "Need #";
+            Debug.LogError("Need number");
+        }
+        Debug.Log(valgen + " " + val1 + " " + val2 + " " + val3);
+    }
+
+}
---
+++ Assets/Scripts/Calculation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculation : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Button calculateButton;
+    public Dropdown Gender_input;
+    public InputField input_1;
+    public InputField input_2;
+    public InputField input_3;
+    public Text CalculationOuput;
+
+    private double val1;
+    private double val2;
+    private double val3;
+    void Start()
+    {
+        Button calcbtn = calculateButton.GetComponent<Button>();
+        calcbtn.onClick.AddListener(TaskOnClick);
+        Dropdown gender = Gender_input.GetComponent<Dropdown>();
+        InputField weight = input_1.GetComponent<InputField>();
+        InputField age = input_2.GetComponent<InputField>();
+        InputField height = input_3.GetComponent<InputField>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Button calcbtn = calculateButton.GetComponent<Button>();
+        calcbtn.onClick.AddListener(TaskOnClick);
+    }
+    private void TaskOnClick()
+    {
+        try{
+            val1 = double.Parse(input_1.text);
+            val2 = double.Parse(input_2.text);
+            val3 = double.Parse(input_3.text);
+        }
+        catch
+        {
+            CalculationOuput.text = "Need #";
+        }
+
+        if (Gender_input.value == 0)
+        {
+
+            if (val1 == 10)
+            {
+                CalculationOuput.text = "10male";
+            }
+        }
+        else if (Gender_input.value == 1)
+        {
+        }
+        else
+        {
+
+        }
+    }
+}
